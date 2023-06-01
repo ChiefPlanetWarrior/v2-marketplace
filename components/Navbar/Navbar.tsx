74dc89f51d26228074c2368d6aae01c06521d00a
Chief Planet Warrior <ps2o@ps2o.io>
2023-05-31 21:30:43 -0400

    modified:   Restored Project 	modified:   components/Navbar/Navbar.tsx 	new file:   pages/api/auth/[...thirdweb].tsx 	new file:   pages/dashboard.tsx 	modified:   pages/index.tsx 	new file:   pages/subDAO.tsx

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,7 +2,68 @@ import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./Navbar.module.css";
+import React, { useState } from "react";
+import Modal from "react-modal";
 
+/**
+ * AboutDialog component
+ */
+const AboutDialog = ({ isOpen, onClose }) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      contentLabel="About"
+      ariaHideApp={false}
+      style={{
+        content: {
+          backgroundColor: "var(--color-background-dark)",
+          color: "black",
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          transform: "translate(-50%, -50%)",
+          border: "none",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.3)",
+          borderRadius: "4px",
+          padding: "20px",
+        },
+        overlay: {
+          backgroundColor: "darkgrey",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+      }}
+    >
+      <h2>VerdeVentures</h2>
+      <p>
+        A trailblazing global marketplace committed to combating climate
+        change through unique initiatives. We offer digital tokens that represent
+        fractionalized ownership in diverse, revenue-generating climate change
+        initiatives. <br></br><br></br>By merging blockchain technology with environmental
+        activism, VerdeVentures democratizes access to sustainable investments
+        and empowers individuals to make a tangible impact. <br></br><br></br>Our platform is more
+        than just a marketplace; it&apos;s the Path to a global community of
+        forward-thinking Climate Warriors passionate about shaping a greener, more
+        sustainable future.
+
+            <div className={styles.link}>
+              <Link href="https://gateway.pinata.cloud/ipfs/QmTmFcqAb4rVkRCvsYzr2sY2eyKxq4teyHJqpDZgSbGpN4">
+                Privacy Policy
+              </Link>
+            </div>
+
+      </p>
+      <h2> <p style={{ textAlign: "center" }}>Own The <span style={{ color: "green" }}>Green</span> Future!</p></h2>
+
+
+
+      <button onClick={onClose}>Close</button>
+    </Modal>
+  );
+};
 
 /**
  * Navigation bar that shows up on all pages.
@@ -10,108 +71,68 @@ import styles from "./Navbar.module.css";
  */
 export function Navbar() {
   const address = useAddress();
+  const [isAboutDialogOpen, setIsAboutDialogOpen] = useState(false);
+
+  const openAboutDialog = () => {
+    setIsAboutDialogOpen(true);
+  };
+
+  const closeAboutDialog = () => {
+    setIsAboutDialogOpen(false);
+  };
 
   return (
+
     <div className={styles.navContainer}>
       <nav className={styles.nav}>
         <div className={styles.navLeft}>
           <div className={`${styles.homeLink} ${styles.navLeft}`}>
             <Link href="/">
               <Image
-                  src="/Primary PS2O Logo.png"
-                  width={48}
-                  height={48}
-                  alt="PS2O Logo"
-                />
+                src="/Primary PS2O Logo.png"
+                width={48}
+                height={48}
+                alt="PS2O Logo"
+              />
             </Link>
           </div>
-          <div className={styles.navLeft}>
+          <div className={styles.navRight}>
             <div className={styles.link}>
-              <Link href="/exchange">
-                Exchange
+              <Link href="/subDAO">
+                subDAO
+              </Link>
+            </div>
+          </div>
+          <div className={styles.navRight}>
+            <div className={styles.link}>
+              <Link href="https://links.geneva.com/invite/966c608a-7309-4a6f-9d7a-2a7cd20f7551">
+                Chat
               </Link>
             </div>
           </div>
           <div className={styles.navLeft}>
             <div className={styles.link}>
-              <Link href="/claim">
-                Claim
+              <Link href="/exchange">Exchange</Link>
+            </div>
+          </div>
+          <div className={styles.navRight}>
+            <div className={styles.link}>
+              <Link href="/dashboard">
+                Dashboard
               </Link>
             </div>
           </div>
           <div className={styles.navLeft}>
             <div className={styles.link}>
-              <Link href="https://gateway.pinata.cloud/ipfs/QmNYf1Z6LUDSqRLD6ThHVwDVbxE4RCJyoHXtjuawMZa8YZ">
+              <Link href="https://bafybeidayasqwawuaeyevuo2s47akabsondmb5xowuxnbn6pltv6tya4u4.ipfs.dweb.link/">
                 Greenpaper
               </Link>
             </div>
           </div>
-          
-          <div className={styles.navRight}>
-            <div className={styles.link}>
-              <Link href="https://gateway.pinata.cloud/ipfs/QmTmFcqAb4rVkRCvsYzr2sY2eyKxq4teyHJqpDZgSbGpN4">
-               Privacy
-              </Link>
+          <div className={styles.navLeft}>
+            <div className={styles.link} onClick={openAboutDialog}>
+              About
             </div>
-          </div>
-        </div>
-
-        <div className={styles.navRight}>
-          <div className={styles.navConnect}>
-            <ConnectWallet theme="dark" btnTitle="Connect Wallet" />
-          </div>
-          {address && (
-            <div className={styles.link}>
-              <Link href={`/profile/${address}`}>
-                
-                  <Image
-                    className={styles.profileImage}
-                    src="/user-icon.png"
-                    width={42}
-                    height={42}
-                    alt="Profile"
-                  />
-                
-              </Link>
-            </div>
-          )}
-        </div>
-      </nav>
-    </div>
-  );
-}
-export default Navbar;
-/**
- *
- * https://gateway.pinata.cloud.ipfs/QmTmFcqAb4rVkRCvsYzr2sY2eyKxq4teyHJqpDZgSbGpN4
-import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
-import Image from "next/image";
-import Link from "next/link";
-import styles from "./Navbar.module.css";
-
-export function Navbar() {
-  const address = useAddress();
-
-  return (
-    <div className={styles.navContainer}>
-      <nav className={styles.nav}>
-        <div className={styles.navLeft}>
-          <Link href="/" className={`${styles.homeLink} ${styles.navLeft}`}>
-            <Image
-              src="/logo.png"
-              width={48}
-              height={48}
-              alt="NFT marketplace sample logo"
-            />
-          </Link>
-
-          <div className={styles.navMiddle}>
-            <Link href="/buy" className={styles.link}>
-              Buy
-            </Link>
-            <Link href="/sell" className={styles.link}>
-              Sell
-            </Link>
           </div>
         </div>
 
@@ -132,10 +153,9 @@ export function Navbar() {
           )}
         </div>
       </nav>
+      <AboutDialog isOpen={isAboutDialogOpen} onClose={closeAboutDialog} />
     </div>
   );
 }
 
-
-
-*/
+export default Navbar;
