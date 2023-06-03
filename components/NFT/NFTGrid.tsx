02a1ca6e7d45e8dcf1f19736ac017f4e318b9d76
Chief Planet Warrior <ps2o@ps2o.io>
2023-06-02 21:36:05 -0400

    Initial commit

--- components/NFT/NFTGrid.tsx
+++ components/NFT/NFTGrid.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { NFT_COLLECTION_ADDRESS } from "../../const/contractAddresses";
 import Skeleton from "../Skeleton/Skeleton";
 import NFT from "./NFT";
-import styles from "../../styles/Claim.module.css";
+import styles from "../../styles/SubDAO.module.css";
 
 type Props = {
   isLoading: boolean;
